e4d5d634a59600ae7b2bfd23ae96456704afa3ae
Fatima Jamil <fatimajamil@192.168.1.2>
2025-11-08 18:55:12 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  origin: ['http://localhost:5173', 'http://localhost:3000',process.env.FRONTEND_URL], 
+  
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
